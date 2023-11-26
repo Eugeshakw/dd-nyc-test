c0ebe5316e86aecbcac1e0597c3ba81f67db5cf7
Eugeshakw <eugeshakw@gmail.com>
2023-11-26 23:11:14 +0200

    uppdate

--- slider.js
+++ slider.js
@@ -11,18 +11,35 @@ const btnLeft = document.querySelector('#btn-left');
 const imgSlideList = document.querySelector('.img-slide-list').children;
 const txtSlideList = document.querySelector('.txt-slide-list').children;
 const txtAnimation = document.querySelectorAll('.slide-animate');
+const numberPagination = document.querySelector('#number')
+
 let currentSlideIndex = 0;
 
 btnRight.addEventListener('click', handleRightClick);
 btnLeft.addEventListener('click', handleLeftClick);
 btnLeft.disabled = true;
 txtSlideList[0].classList.add('img-slider-opacityTrue');
-btnLeft.firstElementChild.classList.add('btn-disabled')
+btnLeft.firstElementChild.classList.add('btn-disabled');
+numberPagination.classList.remove('number-disabled');
+
+
+
 
 function handleRightClick() {
     currentSlideIndex = (currentSlideIndex + 1) % imgSlideList.length;
+    txtSlideList[currentSlideIndex].classList.remove('img-slider-opacityTrue');
+    
+    
+    numberPagination.classList.add('number-disabled');
+   
+    setTimeout(() => {
+        numberPagination.textContent = parseInt(numberPagination.textContent) + 1
+        numberPagination.classList.remove('number-disabled');
+    }, 250)
+
     updateSlide();
     updateButtonState();
+    
     console.log('Switched to the next image');
     btnLeft.firstElementChild.classList.remove('btn-disabled')
     imgSlideList[0].classList.add('img-slider-opacityFalse')
@@ -30,8 +47,17 @@ function handleRightClick() {
 
 function handleLeftClick() {
     currentSlideIndex = (currentSlideIndex - 1 + imgSlideList.length) % imgSlideList.length;
-    updateSlide();
-    updateButtonState();
+
+        numberPagination.classList.add('number-disabled');
+
+    setTimeout(() => {
+        numberPagination.textContent = parseInt(numberPagination.textContent) - 1
+        numberPagination.classList.remove('number-disabled');
+    }, 250)
+
+        updateSlide();
+        updateButtonState();
+        console.log('timed out');
     console.log('Switched to the previous image');
     btnRight.firstElementChild.classList.remove('btn-disabled')
 }
@@ -40,9 +66,15 @@ function updateSlide() {
     for (let i = 0; i < imgSlideList.length; i++) {
         if (i === currentSlideIndex) {
             imgSlideList[i].classList.add('img-slider-opacityTrue');
-            txtSlideList[i].classList.add('img-slider-opacityTrue');
-            txtAnimation[i].classList.add('slide-up-wrap');
-            txtAnimation[i].addEventListener('animationend', removeSlideUpWrap);
+            // txtSlideList[i].classList.add('img-slider-opacityTrue');
+
+            setTimeout(()=> {
+                txtSlideList[i].classList.add('img-slider-opacityTrue');
+                txtAnimation[i].classList.add('slide-up-wrap');
+                txtAnimation[i].addEventListener('animationend', removeSlideUpWrap);
+                
+            },250)
+            
         } else {
             imgSlideList[i].classList.remove('img-slider-opacityTrue');
             txtSlideList[i].classList.remove('img-slider-opacityTrue');
@@ -51,6 +83,7 @@ function updateSlide() {
         }
     }
 }
+
 
 function updateButtonState() {
     btnLeft.disabled = currentSlideIndex === 0;
@@ -65,6 +98,7 @@ function updateButtonState() {
     }
     
 }
+
 
 function removeSlideUpWrap() {
     this.classList.remove('slide-up-wrap');
