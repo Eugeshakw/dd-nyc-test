01d5abd0194640380cab36b477da041eeb3da865
Eugeshakw <eugeshakw@gmail.com>
2023-11-26 16:20:57 +0200

    uppdate

--- slider.js
+++ slider.js
@@ -8,8 +8,13 @@ mySlider.mount()
 
 const btnRight = document.querySelector('#btn-right');
 const btnLeft = document.querySelector('#btn-left');
-const imgBlock = document.querySelectorAll('.img-slider')
+const mainBlock = document.querySelectorAll('.splide__track');
+
+const imgSlider = document.querySelectorAll('.img-slider');
+const imgSliderTwo = document.querySelectorAll('.img-slider-two');
+
 const blockAnimate = document.querySelectorAll('.slide-animate');
+
 console.log(btnRight);
 console.log(btnLeft);
 
@@ -18,24 +23,35 @@ console.log(btnLeft);
 function handleClick()  {
 
 
+
    
     
-    imgBlock.forEach(element => {
-        element.classList.add('img-slider-disabled')
+    // mainBlock.forEach(element => {
+    //     element.classList.add('con-test-main-disabled')
         
-    })
-     
+    // })
+    imgSlider.forEach(element => {
+    element.classList.add('img-slider-opacityFalse');
+    });
+
+    imgSliderTwo.forEach(element => {
+        element.classList.add('img-slider-opacityTrue');
+    });
+    
+    
+
     setTimeout(() => {  
         
         handleOpacity()
         blockAnimationOpen()
-       
-
-   
-    }, 100);
+        
+        
+        // imgAnimationClouse()
+    }, 500);
 
     setTimeout(()=> {
         blockAnimationClouse()
+        
     },1000)
 
     
@@ -48,23 +64,40 @@ function blockAnimationOpen() {
     blockAnimate.forEach(element => {
         element.classList.add('slide-up-wrap');
     });
+
 }
 function blockAnimationClouse() {
          blockAnimate.forEach(element => {
             element.classList.remove('slide-up-wrap'); 
     });
+
         
 }
 
+function imgAnimationClouse() {
+
+    imgSliderTwo.forEach(element => {
+        element.classList.remove('img-slider-opacityTrue');
+    });
+
+    imgSlider.forEach(element => {
+        element.classList.remove('img-slider-opacityFalse');
+    });
+
+    
+}
 
 
 function handleOpacity() {
     
-    imgBlock.forEach(element => {
-        element.classList.remove('img-slider-disabled')
+    mainBlock.forEach(element => {
+        element.classList.remove('con-test-main-disabled')
     })
 
 }
 
 btnRight.addEventListener('click', handleClick);
 btnLeft.addEventListener('click', handleClick);
+
+
+
